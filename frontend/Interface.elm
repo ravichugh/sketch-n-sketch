183a5a88464c856e6ef1da9419913140cdf075db
Mitch Spradlin <mitch@spradlin.net>
2015-04-22 12:27:18 -0500

    Rudimentary text box, strange compilation error

--- frontend/Interface.elm
+++ frontend/Interface.elm
@@ -9,8 +9,14 @@ import Sync
 import Utils
 
 import String
-import Graphics.Element as GE
+import Graphics.Element as GE 
 import Signal
+
+import Window
+import Html
+import Html.Attributes
+import Html.Events
+
 
 -- Model --
 type alias Model = { code : String
@@ -22,14 +28,35 @@ initModel = { code = ""
 type Event = CodeUpdate String
            | OutputUpdate String
 
+events : Signal.Mailbox Event
+events = Signal.mailbox <| CodeUpdate ""
+
 -- Update --
 upstate : Event -> Model -> Model
-upstate evt old = initModel
+upstate evt old = case evt of
+    UpdateCode newcode -> { old | code <- newcode }
+    _ -> old
 
 -- View --
+codeBox : String -> Html.Html
+codeBox codeText =
+    Html.textarea
+        [ Html.Attributes.id "codeBox"
+        , Html.Attributes.value codeText
+        , Html.Events.on "input" Html.Events.targetValue
+            (Signal.message events << CodeUpdate)
+        ]
+        []
+
 view : (Int, Int) -> Model -> GE.Element
-view (w,h) model = GE.spacer w h
+view (w,h) model = GE.flow GE.right [ Html.toElement (w // 2) h
+                                        <| codeBox model.code
+                                    ]
 
 -- Main --
 main : Signal Element
-main = Singal.constant <| GE.spacer 10 10
+main = let sigModel = Signal.foldp upstate initState
+                        <| Signal.mergeMany
+                            [ events.signal
+                            ]
+       in Signal.map2 view Window.dimensions sigModel
