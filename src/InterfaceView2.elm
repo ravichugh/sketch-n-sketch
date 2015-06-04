22a34e7bc959c576151441051d3c62f97175dbd8
Ravi Chugh <rchugh@cs.uchicago.edu>
2015-06-04 16:29:34 -0500

    moving params to new Config module

--- src/InterfaceView2.elm
+++ src/InterfaceView2.elm
@@ -14,6 +14,7 @@ import MicroTests
 import InterfaceUtils exposing (..)
 import LangSvg exposing (IndexedTree, NodeId, ShapeKind, Attr, toNum, toNumTr, addi)
 import Examples
+import Config exposing (params)
 
 import VirtualDom
 
@@ -208,56 +209,6 @@ makeZonesPoly showZones shape id l =
   if | shape == "polygon" -> zInterior :: (zLines ++ zPts)
      | firstEqLast pts    -> zInterior :: (zLines ++ zPts)
      | otherwise          -> zLines ++ zPts
-
-
---------------------------------------------------------------------------------
--- User Interface Layout
-
--- Configuration Parameters
--- Only constants in this record may be changed.
---
-params =
-  { strVersion = "v0.0"
-  , debugLayout = False    -- displays colors for high-level layout structure
-  , wGut = 10              -- width of left/right side gutters (spans entire height)
-  , topSection =
-     { h = 40              -- height of top space
-     , wBtnO = 180         -- width...
-     , hBtnO = 25          -- ... and height of orientation button
-     , wJunk = 225         -- gap between title and orientation button
-     }
-  , botSection =
-     { h = 30              -- height of bot space
-     }
-  , mainSection =
-     { widgets =           -- Render/Sync buttons; Mode/Tests dropdowns
-        { wBtn = 100
-        , hBtn = 25
-        , font = "Tahoma, sans-serif"
-        , fontSize = "10pt"
-        }
-     , vertical =
-        { hExtra = 15      -- extra vertical space around widgets
-        , wGut = 10        -- width of gutters in between code/widgets/canvas
-        }
-     , horizontal =
-        { wExtra = 15      -- extra horizontal space around widgets
-        , hGut = 10        -- height of gutters in between code/widgets/canvas
-        }
-     , canvas =
-        { border = "0px solid darkGray"
-        }
-     , codebox =
-        { border = "none"
-        , font = "Courier, monospace"
-        , fontSize = "12pt"
-        }
-     }
-  }
-
--- End Configuration Parameters
-------------------------------------------------------------------------------
-
 
 strTitle = "sketch-n-sketch " ++ params.strVersion
 
