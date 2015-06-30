e8b86457f85d12bf5761f48464981e98cecbb49e
Jacob Albers <jacobleealbers@gmail.com>
2015-06-30 12:41:54 -0500

    fixed issue with tiled frank lloyd wright

--- src/Examples.elm
+++ src/Examples.elm
@@ -370,59 +370,49 @@ flw1 = "
                [(xoff  6) (yoff  5) (/ (+ wbox hbox) 2)]])))))))))))))))
 "
 
---flw2 = "
---  ;
---  ; A Frank Lloyd Wright design based on:
---  ; http://www.glass-by-design.com/images3/skylight3.jpg
---  ;
---  ; This is a tiled version of that design
---  ;
---  (let [x0 y0 w h max] [69 55 190 301 10!]
---  (let wbox (/ w 10!)
---  (let hbox (/ h 10!)
---  (let xoff (\\(n x) (* x (+ x0 (* wbox n))))
---  (let yoff (\\(n y) (* y (+ y0 (* hbox n))))
---  (let blkline (\\[[a b] [c d]] (line 'black' 3 a b c d))
---  (let redpoly
---    (\[a b x y]
---      (polygon
---        'red'
---        'black'
---        3
---        [[(xoff  a x) (yoff  a y)]
---         [(xoff  a x) (yoff  b y)]
---         [(xoff  b x) (yoff  b y)]
---         [(xoff  b x) (yoff  a y)]]))
---  (let dimension [0! 1 2 2.9 2.4 1.5 9.1 7.9 8.2 8.7 10!]
---  (let [p0 p1 p2 p3 p4] [0 1 2 2.9 5]
---  (let singletile
---    (\\[x y]
---      (let verticals
---        (zip
---          (map (\\n [(xoff  n) y0]) dimension)
---          (map (\\n [(xoff  n) (+ y0 (* hbox max))]) dimension))
---      (let horizontals
---        (zip
---          (map (\\n [x0 (yoff  n)]) dimension)
---          (map (\\n [(+ x0 (* wbox max)) (yoff  n)]) dimension))
---      (append
---          (map blkline (append verticals horizontals))
---          (append
---            (append
---              (map redpoly [[p0 p1] [p1 p2] [p2 p3] [p3 p4]])
---              (map (\\[x y] (ellipse 'blue' x y (* wbox 4) hbox)) [[(xoff  5 x) (yoff  9)]]))
---            (map
---              (\[x y r] (circle 'yellow' x y r))
---              [[(xoff  6 x) (yoff  1.75 y) (+ wbox hbox)]
---               [(xoff  6 x) (yoff  7 y) (/ (+ wbox hbox) 4)]
---               [(xoff  6 x) (yoff  5 y) (/ (+ wbox hbox) 2)]])))))))
---  (let grid (cartProd (range 0! 3!) (range 0! 1!))
---    (svg 
---      (cons
---        (rect 'white' (- x0 10!) (- y0 10!) (+ w 20!) (+ h 20!))
---        (map singletile grid)
---        ))))))))))
---"
+flw2 = "
+  ;
+  ; A Frank Lloyd Wright design based on:
+  ; http://www.glass-by-design.com/images3/skylight3.jpg
+  ;
+  ; This is a tiled version of that design
+  ;
+  (let [x0 y0 w h max] [69 55 200 320 10!]
+  (let wbox (/ w 10!)
+  (let hbox (/ h 10!)
+  (let xoff (\\(n x) (+ x0 (+ (* x w) (* wbox n))))
+  (let yoff (\\(n y) (+ y0 (+ (* y h) (* hbox n))))
+  (let blkline (\\[[a b] [c d]] (line 'black' 3 a b c d))
+  (let redpoly
+    (\\[a b x y]
+      (polygon
+        'red'
+        'black'
+        3
+        [[(xoff a x) (yoff a y)]
+         [(xoff a x) (yoff b y)]
+         [(xoff b x) (yoff b y)]
+         [(xoff b x) (yoff a y)]]))
+  (let dimension [0! 4 5 6 7 10!]
+  (let [p0 p1 p2 p3 p4] [0 1 2 2.9 5]
+  (let singletile
+    (\\[x y] 
+      (let verticals
+        (zip
+          (map (\\n [(xoff n x) (+ y0 (* y h))]) dimension)
+          (map (\\n [(xoff n x) (+ y0 (* (+ y 1) h))]) dimension))
+      (let horizontals
+        (zip
+          (map (\\n [(+ x0 (* x w)) (yoff n y)]) dimension)
+          (map (\\n [(+ x0 (* (+ 1 x) w)) (yoff n y)]) dimension))
+        (append
+          (map blkline (append verticals horizontals))
+          (map redpoly [[p0 p1 x y] [p1 p2 x y] [p2 p3 x y] [p3 p4 x y]])))))
+  (let grid (cartProd [0! 1! 2!] [0! 1!])
+    (svg 
+      (cons (rect 'white' (- x0 10!) (- y0 10!) (+ (* 3 w) 20!) (+ (* 2 h) 20!))
+        (concatMap singletile grid))))))))))))))
+"
 
 chicago = "
   ;
@@ -660,6 +650,7 @@ examples =
   , makeExample "Chicago Flag" chicago
   , makeExample "French Sudan Flag" frenchSudan
   , makeExample "Frank Lloyd Wright" flw1
+  , makeExample "F.L. Wright Tiled" flw2
   , makeExample "Ferris Wheel" ferris
   , makeExample "Clique" clique
   , makeExample "Pie Chart" piechart
