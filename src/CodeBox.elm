58fdd3c6006b2134fbc5437bcfcd61f12fa2bcbe
Mitchell Henry Spradlin <mitch@spradin.net>
2015-08-04 14:40:12 -0500

    Adding logging back

--- src/CodeBox.elm
+++ src/CodeBox.elm
@@ -61,7 +61,7 @@ type alias AceMessage = { evt : String
 --interpretAceHtml ahtml = GE.spacer 0 0
 
 interpretAceEvents : AceMessage -> Event
-interpretAceEvents amsg = case {-Debug.log "got\n"-} amsg.evt of
+interpretAceEvents amsg = case Debug.log "got\n" amsg.evt of
     "AceCodeUpdate" -> InterfaceModel.UpdateModel <|
         \m -> { m | code <- amsg.strArg
                   , cursorPos <- amsg.cursorArg 
