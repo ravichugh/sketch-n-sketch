ed1c7589e7b5a4af0e8824cae72f27e69cad524d
Mitch Spradlin <mitch@spradlin.net>
2015-08-11 15:44:55 -0500

    Added keypresses back

--- src/Main.elm
+++ src/Main.elm
@@ -34,7 +34,10 @@ combinedEventSig =
       |> Signal.map (\(x,y) -> y)
       |> Signal.map2 adjustCoords Window.dimensions
       |> Signal.map Model.MousePos
-    , Signal.map interpretAceEvents theTurn 
+    , Signal.map interpretAceEvents theTurn
+    , Signal.map
+      (Model.KeysDown << List.sort << Set.toList)
+      Keyboard.keysDown
     ]
 
 main : Signal Element
