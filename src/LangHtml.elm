fd8ce76bc40f755ea3ad0761ae702f78b881aa33
Jacob Albers <jacobleealbers@gmail.com>
2015-08-27 15:27:25 -0500

    added imports from LangSvg

--- src/LangHtml.elm
+++ src/LangHtml.elm
@@ -1,3 +1,30 @@
 -- LangHtml.elm
+
+module LangHtml where
+-- module LangSvg (valToHtml, valToIndexedTree, printIndexedTree) where
+
+import Html
+import Html.Attributes as HA
+import Svg
+import Svg.Attributes as A
+import VirtualDom
+
+-- in Svg.elm:
+--   type alias Svg = VirtualDom.Node
+--   type alias Attribute = VirtualDom.Property
+
+-- in Html.elm:
+--   type alias Html = VirtualDom.Node
+
+import Debug
+import Set
+import String
+import Char
+import Dict exposing (Dict)
+import ColorNum
+
+import Lang exposing (..)
+import Utils
+
 -- Noodling about what it would take to replace LangSvg with this, and have the
 -- output be retargeted to elm-html instead of elm-svg.
