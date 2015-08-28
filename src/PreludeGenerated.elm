9c4c14deb22caa0f922d4c06f658b7eb187fab54
Mitchell Henry Spradlin <mitch@spradin.net>
2015-08-28 16:47:27 -0500

    Adding generated Prelude and Examples

--- src/PreludeGenerated.elm
+++ src/PreludeGenerated.elm
@@ -16,8 +16,8 @@ prelude =
 ;; A function that always returns the same value a, regardless of b
 (def always (\\(x _) x))
 
-;; compose : (a -> b) -> (b -> c) -> (a -> c)
-;; Composes two functions
+;; compose : (a -> b)-> (b -> c) -> (a -> c)
+;; Composes two functions together
 (def compose (\\(f g) (\\x (f (g x)))))
 
 ;; fst : List a -> a
@@ -41,12 +41,12 @@ prelude =
     (_                 []))))
 
 ;; foldl : (a -> b -> b) -> b -> List a -> b
-;; Takes a function, an accumulator, and a list as input and reduces from the left with the given function
+;; Takes a function, an accumulator, and a list as input and reduces using the function from the left
 (defrec foldl (\\(f acc xs)
   (case xs ([] acc) ([x|xs1] (foldl f (f x acc) xs1)))))
 
 ;; foldl : (a -> b -> b) -> b -> List a -> b
-;; Takes a function, an accumulator, and a list as input and reduces from the left with the given function
+;; Takes a function, an accumulator, and a list as input and reduces using the function from the right
 (defrec foldr (\\(f acc xs)
   (case xs ([] acc) ([x|xs1] (f x (foldr f acc xs1))))))
 
@@ -150,7 +150,7 @@ prelude =
 (def not (\\b (if b false true)))
 
 ;; implies : Bool -> Bool -> Bool
-;; Given two bools, returns a bool regarding 'if the first argument is true, then the second argument is as well'
+;; Given two bools, returns a bool regarding if the first argument is true, then the second argument is as well
 (def implies (\\(p q) (if p q true)))
 
 ;; clamp : Number -> Number -> Number -> Number
@@ -183,60 +183,34 @@ prelude =
 (def parens (delimit '(' ')'))
 
 ;
-; HTML Manipulating Functions
+; HTML manipulating functions
 ;
 
-(def html (\\objects ['html' [] objects]))
+(def body (\\(attrs children) ['body' attrs children]))
 
 (def head (\\(attrs children) ['head' attrs children]))
 
-(def body (\\(attrs children) ['body' attrs children]))
+(def html (\\(attrs children) ['html' attrs children]))
+
+; A basic HTML document, handy for simple documents
+(def basicDoc (\\(attrs children) 
+  (html [] 
+    [ (head [] []) 
+      (body [ ['width' '100%'] ['height' '100%'] ] children) ] ) ) )
 
 (def p (\\(attrs children) ['p' attrs children]))
 
 (def div (\\(attrs children) ['div' attrs children]))
 
-;; a similar function to http://package.elm-lang.org/packages/evancz/elm-html/4.0.1/Html-Attributes#style
-;; to help easily add and remove stylings
-(def style (\\(attrs)
-  (let boundKVs
-    (map (\\s (joinStrings ': ' s)) attrs )      
-  ['style'
-    (joinStrings '; ' (snoc '' boundKVs))]
-  )))
+(def text (\\string ['TEXT' string]))
 
+(def span (\\(attrs children) ['span' attrs children]))
 
-;; addAttr : Node-> Attribute -> Node
-;; argument order - node, new attribute
-;; Add a new attribute to a given node
-(def addAttr (\\([nodeKind oldAttrs children] newAttr)
-  [nodeKind (snoc newAttr oldAttrs) children]))
-
-;; text : Number -> Number -> String -> Shape
-;; argument order - x, y, string
-;; place a text string with top left corner at (x,y) - with default color & font
-(def text (\\(x y s)
-   ['text' [['x' x] ['y' y] ['style' 'fill:black']
-            ['font-family' 'Tahoma, sans-serif']]
-           [['TEXT' s]]]))
-
-;; updateCanvas : SVG -> SVG -> SVG
-;; updates an SVG by comparing differences with another SVG
-;; Note: accDiff pre-condition: indices in increasing order
-;; (so can't just use foldr instead of reverse . foldl)
-(def updateCanvas (\\([_ svgAttrs oldShapes] diff)
-  (let oldShapesI (zip (list1N (len oldShapes)) oldShapes)
-  (let initAcc [[] diff]
-  (let f (\\([i oldShape] [accShapes accDiff])
-    (case accDiff
-      ([]
-        [(cons oldShape accShapes) accDiff])
-      ([[j newShape] | accDiffRest]
-        (if (= i j)
-          [(cons newShape accShapes) accDiffRest]
-          [(cons oldShape accShapes) accDiff]))))
-  (let newShapes (reverse (fst (foldl f initAcc oldShapesI)))
-    ['svg' svgAttrs newShapes]))))))
+;; addAttr : Shape-> Attribute -> Shape
+;; argument order - shape, new attribute
+;; Add a new attribute to a given Shape
+(def addAttr (\\([shapeKind oldAttrs children] newAttr)
+  [shapeKind (snoc newAttr oldAttrs) children]))
 
 ; \"constant folding\"
 (def twoPi (* 2 (pi)))
@@ -259,30 +233,6 @@ prelude =
   (let pts (nPointsOnUnitCircle n rot)
   (map (\\[x y] [(+ cx (* x r)) (+ cy (* y r))]) pts))))
 
-;; nStar : color -> color -> Number -> Number -> Number -> Number -> Number -> Number -> Number -> Shape
-;; argument order -
-;; fill color - interior color of star
-;; stroke color - border color of star
-;; width - thickness of stroke
-;; points - number of star points
-;; len1 - length from center to one set of star points
-;; len2 - length from center to other set of star points (either inner or outer compared to len1)
-;; rot - degree of rotation
-;; cx - x-coordinate of center position
-;; cy - y-coordinate of center position
-;; Creates stars that can be modified on a number of parameters
-(def nStar (\\(fill stroke w n len1 len2 rot cx cy)
-  (let pti (\\[i len]
-    (let anglei (+ (- (/ (* i (pi)) n) rot) halfPi)
-    (let xi (+ cx (* len (cos anglei)))
-    (let yi (+ cy (neg (* len (sin anglei))))
-      [xi yi]))))
-  (let lengths
-    (map (\\b (if b len1 len2))
-         (concat (repeat n [true false])))
-  (let indices (list0N (- (* 2! n) 1!))
-    (polygon fill stroke w (map pti (zip indices lengths))))))))
-
 ;; zones : String -> Shape -> Shape
 ;; Add a string-specified type of zones to a given shape
 (def zones (\\s (map (\\shape (addAttr shape ['zones' s])))))
@@ -295,69 +245,8 @@ prelude =
 ;; Turn all zones to basic for a given list of shapes except for the first shape
 (def basicZonesTail (\\[hd | tl] [hd | (zones 'basic' tl)]))
 
-;; hSlider_ : Bool -> Int -> Int -> Int -> Num -> Num -> Str -> Num
-;; -> [Num (List Svg)]
-;; argument order - dropBall roundInt xStart xEnd y minVal maxVal caption curVal
-;; dropBall - Determines if the slider ball continues to appear past the edges of the slider
-;; roundInt - Determines whether to round to Ints or not
-;; xStart - left edge of slider
-;; xEnd - right edge of slider
-;; y - y positioning of entire slider bar
-;; minVal - minimum value of slider
-;; maxVal - maximum value of slider
-;; caption - text to display along with the slider
-(def hSlider_ (\\(dropBall roundInt xStart xEnd y minVal maxVal caption curVal)
-  (let [rPoint wLine rBall] [4! 3! 10!]
-  (let [xDiff valDiff] [(- xEnd xStart) (- maxVal minVal)]
-  (let xBall (+ xStart (* xDiff (/ (- curVal minVal) valDiff)))
-  (let xBall_ (clamp xStart xEnd xBall)
-  (let rBall_ (if dropBall (if (= xBall_ xBall) rBall 0) rBall)
-  (let val
-    (let val_ (clamp minVal maxVal curVal)
-    (if roundInt (round val_) val_))
-  (let shapes1
-    [ (line 'black' wLine xStart y xEnd y)
-      (circle 'black' xStart y rPoint)
-      (circle 'black' xEnd y rPoint)
-      (text (+ xEnd 10) (+ y 5) (+ caption (toString val))) ]
-  (let shapes2
-    [ (circle 'black' xBall y rBall_) ]
-  [val (append (zones 'none' shapes1)
-               (zones 'basic' shapes2))]))))))))))
-
-;; As hSlider_, but dropBall disappears when it passes then end of a slider
-(def hSlider (hSlider_ true))
-
-;; button_ : Bool -> Number -> Number -> String -> Number -> SVG
-;; Similar to sliders, but just has boolean values
-(def button_ (\\(dropBall xStart y caption xCur)
-  (let [rPoint wLine rBall wSlider] [4! 3! 10! 70!]
-  (let xEnd (+ xStart wSlider)
-  (let xBall (+ xStart (* xCur wSlider))
-  (let xBall_ (clamp xStart xEnd xBall)
-  (let rBall_ (if dropBall (if (= xBall_ xBall) rBall 0) rBall)
-  (let val (< xCur 0.5)
-  (let shapes1
-    [ (circle 'black' xStart y rPoint)
-      (circle 'black' xEnd y rPoint)
-      (line 'black' wLine xStart y xEnd y)
-      (text (+ xEnd 10) (+ y 5) (+ caption (toString val))) ]
-  (let shapes2
-    [ (circle (if val 'darkgreen' 'darkred') xBall y rBall_) ]
-  [val (append (zones 'none' shapes1)
-               (zones 'basic' shapes2))]))))))))))
-
-;; As button, but set initially to true
-(def button (button_ true))
-
-;; rotate : Shape -> Number -> Number -> Number -> Shape
-;; argument order - shape, rot, x, y
-;; Takes a shape rotates it rot degrees around point (x,y)
-(def rotate (\\(shape n1 n2 n3)
-  (addAttr shape ['transform' [['rotate' n1 n2 n3]]])))
-
 ; 0
-['svg' [] []]
+['html' [] []]
 
 "
 
