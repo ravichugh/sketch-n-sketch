4755644a54d7f3a7e753b05fe0379855afb0f45a
Ravi Chugh <rchugh@cs.uchicago.edu>
2015-12-11 16:16:49 -0600

    drawing point/line attribute selection zones

--- src/InterfaceModel.elm
+++ src/InterfaceModel.elm
@@ -60,6 +60,7 @@ type alias Model =
   , errorBox : Maybe String
   , genSymCount : Int
   , newShapeKind : Maybe ShapeKind
+  , selectedAttrs : Set.Set (NodeId, ShapeKind, String)
   }
 
 type Mode
@@ -113,10 +114,10 @@ type alias PossibleChange = (Exp, Val, RootedIndexedTree, Code)
 -- InterfaceStorage is more succinct (Enum typeclass would be nice here...)
 type alias ShowZones = Int
 
-showZonesModes = 5
+showZonesModes = 6
 
-(showZonesNone, showZonesBasic, showZonesRot, showZonesColor, showZonesDel) =
-  Utils.unwrap5
+(showZonesNone, showZonesBasic, showZonesSelect, showZonesRot, showZonesColor, showZonesDel) =
+  Utils.unwrap6
     [ 0 .. (showZonesModes - 1) ]
 
 type Caption
@@ -263,5 +264,6 @@ sampleModel =
     , errorBox      = Nothing
     , genSymCount   = 0
     , newShapeKind  = Nothing
+    , selectedAttrs = Set.empty
     }
 
