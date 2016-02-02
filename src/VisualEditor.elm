a322694b239723eb4de77343299b927e7107a24f
Ravi Chugh <rchugh@cs.uchicago.edu>
2016-02-02 10:50:48 -0600

    experimenting: incr/decr widgets; reparse button

--- src/VisualEditor.elm
+++ src/VisualEditor.elm
@@ -7,7 +7,7 @@ import Json.Decode as Decode
 import String
 import Dict
 import Signal exposing (Mailbox, mailbox)
-import Time
+import Time exposing (Time)
 
 import ExamplesGenerated as Ex
 import Lang exposing (..)
@@ -15,6 +15,10 @@ import LangParser2 as Parser
 import LangUnparser as Unparser
 import OurParser2 exposing (WithInfo, Pos)
 import Utils
+
+------------------------------------------------------------------------------
+
+stopPropagation = { defaultOptions | stopPropagation = True }
 
 ------------------------------------------------------------------------------
 -- Styles
@@ -62,16 +66,26 @@ opUseStyle =
     [ ("background", "brown")
     ] ++ border
 
-literalOuterStyle =
-  Attr.style
-    [ ("background", "gray")
-    , ("padding", "5pt")
-    , ("border-radius", "5pt")
-    , ("cursor", "grab")
-    ]
+literalOuterStyle model padding =
+  let (l1,l2) =
+    -- ( [ ("padding", "5pt")
+    --   , ("border-radius", "5pt")
+    ( [ ("padding", padding)
+      , ("border-radius", "5pt")
+      , ("background", "lightgray")
+      ]
+    , [ ("background", "gray")
+      , ("cursor", "pointer")
+      ])
+  in
+  let l =
+    case model.textChangedAt of
+      Nothing -> l1 ++ l2
+      Just _  -> l1
+  in
+  Attr.style l
 
-literalInnerStyle : Attribute
-literalInnerStyle =
+literalInnerStyle model =
   Attr.style <|
     [ ("background", "yellow")
     , ("cursor", "text")
@@ -81,18 +95,29 @@ literalInnerStyle =
 ------------------------------------------------------------------------------
 -- Events
 
--- this is a simple example, just increments current value by one
-eConstEvent : Float -> Loc -> Attribute
-eConstEvent n loc =
-  let (locid,_,_) = loc in
-  Events.onClick myMailbox.address <| UpdateModel <| \model ->
+updateModelMessage f =
+  (\_ -> Signal.message myMailbox.address (UpdateModel f))
 
+handleAndStop evt f =
+  Events.onWithOptions evt stopPropagation Decode.value (updateModelMessage f)
+
+-- this is a simple example, just increments current value by offset
+eConstEvent : Float -> Loc -> Int -> Attribute
+eConstEvent n loc offset =
+  let (locid,_,_) = loc in
+  handleAndStop "click" <| \model ->
     -- relying on invariant that EId = LocId
-    let lSubst = Dict.singleton locid (n+1) in
+    let lSubst = Dict.singleton locid (n + toFloat offset) in
     let exp' = applyLocSubst lSubst model.exp in
     let code' = Unparser.unparseE exp' in
     { model | exp = exp', code = code' }
 
+-- <span> doesn't have a "change" event
+eTextChange =
+  handleAndStop "click" <| \m ->
+    { m | textChangedAt = Just 0 }
+
+{-
 onClickWithoutPropagation : Signal.Address a -> a -> Attribute
 onClickWithoutPropagation address a = Events.onWithOptions "click" {defaultOptions | stopPropagation = True} Decode.value (\_ -> Signal.message address a)
                                    
@@ -105,6 +130,21 @@ eVarEvent x id =
     let exp' = applyESubst eSubst model.exp in
     let code' = Unparser.unparseE exp' in
     { model | exp = exp', code = code'}
+-}
+
+------------------------------------------------------------------------------
+
+eConstOuterAttrs model n loc padding offset =
+  case model.textChangedAt of
+    Nothing -> [ literalOuterStyle model padding, eConstEvent n loc offset ]
+    Just _  -> [ literalOuterStyle model padding ]
+
+eConstInnerAttrs model =
+  [ Attr.contenteditable True
+  , literalInnerStyle model
+  , eTextChange
+  ]
+
 
 ------------------------------------------------------------------------------
 -- Expression to HTML
@@ -157,50 +197,55 @@ delimit open close startOutside startInside endInside endOutside hs =
 parens = delimit "(" ")"
 brackets = delimit "[" "]"
 
-htmlOfExp : Exp -> Html
-htmlOfExp e = 
+htmlOfExp : Model -> Exp -> Html
+htmlOfExp model e =
+  let recurse = htmlOfExp model in
   let e_ = e.val in
   let e__ = e_.e__ in
   case e__ of
     EConst n loc _ ->
       Html.span
-         [ literalOuterStyle, eConstEvent n loc ]
-         [ Html.span [ literalInnerStyle ] [ Html.text <| toString n ] ]
+         (eConstOuterAttrs model n loc "0 0 0 8pt" (-1))
+         [ Html.span
+              (eConstOuterAttrs model n loc "0 8pt 0 0" 1)
+              [ Html.span
+                   (eConstInnerAttrs model)
+                   [ Html.text <| toString n ] ] ]
     EBase baseVal ->
       case baseVal of
         Bool b -> Html.span [ literalStyle ] [ Html.text <| toString b ]
         String s -> Html.span [ literalStyle ] [ Html.text <| "\'" ++ s ++ "\'" ]
         Star -> Html.span [ literalStyle ] [ Html.text <| toString Star ]
     EOp op es ->
-      let hs = htmlMap htmlOfExp es in
+      let hs = htmlMap recurse es in
       let (h,l) = (Utils.head_ es, Utils.last_ es) in
       Html.span [ basicStyle ] <|
           parens e.start op.start l.end e.end <|
             [ Html.span [ opUseStyle ] <| [ Html.text <| strOp op.val ]]
             ++ space op.end h.start
             ++ hs
-    EVar x -> Html.span [ varUseStyle, eVarEvent x e.val.eid ] [ Html.text x]
+    EVar x -> Html.span [ varUseStyle ] [ Html.text x]
     EFun [p] e1 ->
-      let (h1, h2) = (htmlOfPat p, htmlOfExp e1) in
+      let (h1, h2) = (htmlOfPat model p, recurse e1) in
       let tok = Unparser.makeToken (Unparser.incCol e.start) "\\" in
       Html.span [ basicStyle ] <| parens e.start tok.start e1.end e.end <|
         [ Html.text tok.val ] ++ space tok.end p.start ++ [ h1 ] ++ space p.end e1.start ++ [ h2 ]
     EFun ps e1 ->
       let tok = Unparser.makeToken (Unparser.incCol e.start) "\\" in
-      let (h1, h2) = (htmlMap htmlOfPat ps, htmlOfExp e1) in
+      let (h1, h2) = (htmlMap (htmlOfPat model) ps, recurse e1) in
       Html.span [ basicStyle ] <| parens e.start tok.start e1.end e.end <|
           let (h,l) = (Utils.head_ ps, Utils.last_ ps) in
           [ Html.text tok.val] ++ space tok.end (Unparser.decCol h.start)
              ++ [ Html.text "("] ++ h1 ++ [ Html.text ")"]
                  ++ space (Unparser.incCol l.end) e1.start ++ [ h2 ]
     EApp e1 es ->
-      let (h1, hs) = (htmlOfExp e1, htmlMap htmlOfExp es) in
+      let (h1, hs) = (recurse e1, htmlMap recurse es) in
       let (h,l) = (Utils.head_ es, Utils.last_ es) in
           Html.span [ basicStyle ] <| parens e.start e1.start l.end e.end <|
                [h1] ++ space e1.end h.start ++ hs
              
     ELet Let r p e1 e2 ->
-      let (h1, h2, h3) = (htmlOfPat p, htmlOfExp e1, htmlOfExp e2) in
+      let (h1, h2, h3) = (htmlOfPat model p, recurse e1, recurse e2) in
       let s1 = space p.end e1.start
           s2 = space e1.end e2.start
       in
@@ -216,7 +261,7 @@ htmlOfExp e =
             parens e.start tok.start e2.end e.end <|
                      [ Html.text tok.val] ++ space tok.end p.start ++ rest
     ELet Def r p e1 e2 ->
-      let (h1, h2, h3) = (htmlOfPat p, htmlOfExp e1, htmlOfExp e2) in
+      let (h1, h2, h3) = (htmlOfPat model p, recurse e1, recurse e2) in
       let s1 = space p.end e1.start
           s2 = space e1.end e2.start
       in
@@ -234,9 +279,9 @@ htmlOfExp e =
     EList xs Nothing ->
       case xs of
         [] -> Html.span [ basicStyle ] <| [ Html.text "[]" ]
-        _  -> Html.span [ basicStyle ] <| brackets e.start (Utils.head_ xs).start (Utils.last_ xs).end e.end <| htmlMap htmlOfExp xs
+        _  -> Html.span [ basicStyle ] <| brackets e.start (Utils.head_ xs).start (Utils.last_ xs).end e.end <| htmlMap recurse xs
     EList xs (Just y) ->
-      let (h1, h2) = (htmlMap htmlOfExp xs, htmlOfExp y) in
+      let (h1, h2) = (htmlMap recurse xs, recurse y) in
       let (e1,e2) = (Utils.head_ xs, Utils.last_ xs) in
       let tok1 = Unparser.makeToken e.start "["
           tok2 = Unparser.makeToken e2.end "|"
@@ -246,28 +291,29 @@ htmlOfExp e =
            delimit tok1.val tok2.val tok1.start e1.start e2.end tok2.end (space tok1.end e1.start ++ h1)
                 ++ space tok2.end y.start ++ [ h2 ] ++ space y.end tok3.start ++ [ Html.text tok3.val ]
     EIf e1 e2 e3 ->
-      let (h1,h2,h3) = (htmlOfExp e1, htmlOfExp e2, htmlOfExp e3) in
+      let (h1,h2,h3) = (recurse e1, recurse e2, recurse e3) in
       let tok = Unparser.makeToken (Unparser.incCol e.start) "if" in
       let s1 = space tok.end e1.start
       in
       Html.span [ basicStyle ] <| parens e.start tok.start e3.end e.end <|
-            [ Html.text tok.val] ++ s1 ++ htmlMap htmlOfExp [ e1, e2, e3 ]
+            [ Html.text tok.val] ++ s1 ++ htmlMap recurse [ e1, e2, e3 ]
     EComment s e1 ->
       let white = space (Unparser.incLine e.start) e1.start in
       Html.span [ basicStyle] <|
-            [ Html.text <| ";" ++ s] ++ [ Html.text <| "\n" ] ++ white ++ [ htmlOfExp e1 ] 
+            [ Html.text <| ";" ++ s] ++ [ Html.text <| "\n" ] ++ white ++ [ recurse e1 ] 
     ECase e1 bs ->
       let tok = Unparser.makeToken (Unparser.incCol e.start) "case" in
       let l = Utils.last_ bs in
       Html.span [ basicStyle ] <|
-          parens e.start tok.start l.end e.end <| htmlMap htmlOfBranch bs
+          parens e.start tok.start l.end e.end <| htmlMap (htmlOfBranch model) bs
     _ -> 
       -- let _ = Debug.log "VisualEditor.HtmlOfExp no match :" (toString e) in
       let s = Unparser.unparseE e in
       Html.pre [] [ Html.text s ]
 
-htmlOfPat : Pat -> Html
-htmlOfPat p =
+htmlOfPat : Model -> Pat -> Html
+htmlOfPat model p =
+    let recurse = htmlOfPat model in
     case p.val of
       PVar x _ -> Html.span [ patUseStyle ] [ Html.text x ]
       PConst n -> Html.span [ literalStyle ] [ Html.text <| toString n ]
@@ -275,9 +321,9 @@ htmlOfPat p =
       PList xs Nothing ->
         case xs of
           [] -> Html.span [ basicStyle ] <| [ Html.text "[]" ] 
-          _  -> Html.span [ basicStyle ] <| brackets p.start (Utils.head_ xs).start (Utils.last_ xs).end p.end <| htmlMap htmlOfPat xs
+          _  -> Html.span [ basicStyle ] <| brackets p.start (Utils.head_ xs).start (Utils.last_ xs).end p.end <| htmlMap recurse xs
       PList xs (Just y) ->
-        let (h1, h2) = (htmlMap htmlOfPat xs, htmlOfPat y) in
+        let (h1, h2) = (htmlMap recurse xs, recurse y) in
         let (e1,e2) = (Utils.head_ xs, Utils.last_ xs) in
         let tok1 = Unparser.makeToken p.start "["
             tok2 = Unparser.makeToken e2.end "|"
@@ -287,12 +333,12 @@ htmlOfPat p =
             delimit tok1.val tok2.val tok1.start e1.start e2.end tok2.end (space tok1.end e1.start ++ h1)
                 ++ space tok2.end y.start ++ [ h2 ] ++ space y.end tok3.start ++ [ Html.text tok3.val ]
 
-htmlOfBranch : Branch -> Html
-htmlOfBranch b =
+htmlOfBranch : Model -> Branch -> Html
+htmlOfBranch model b =
   let (p,e) = b.val in
   Html.span [ basicStyle] <|
       parens b.start p.start e.end b.end <|
-             [ htmlOfPat p ] ++ space p.end e.start ++ [ htmlOfExp e ]
+             [ htmlOfPat model p ] ++ space p.end e.start ++ [ htmlOfExp model e ]
 
 
 ------------------------------------------------------------------------------
@@ -305,16 +351,21 @@ type Event
 myMailbox : Mailbox Event
 myMailbox = mailbox (UpdateModel identity)
 
+btnMailbox : Mailbox ()
+btnMailbox = mailbox ()
+
 type alias Model =
   { name : String
   , code : String
   , exp  : Exp
+  , textChangedAt : Maybe Time
   }
 
 initModel =
   { name = Ex.scratchName
   , code = Ex.scratch
   , exp  = Utils.fromOk_ (Parser.parseE Ex.scratch)
+  , textChangedAt = Nothing
   }
 
 upstate : Event -> Model -> Model
@@ -324,7 +375,7 @@ upstate evt model =
     HtmlUpdate code ->
       case Parser.parseE code of
         Err _  -> model
-        Ok exp -> { model | exp = exp, code = code }
+        Ok exp -> { model | exp = exp, code = code, textChangedAt = Nothing }
 
 -- http://stackoverflow.com/questions/32426042/how-to-print-index-of-selected-option-in-elm
 targetSelectedIndex : Decode.Decoder Int
@@ -333,12 +384,6 @@ targetSelectedIndex = Decode.at ["target", "selectedIndex"] Decode.int
 view : Model -> Html
 view model =
   let testString = model.code in
-  {-let testString = "(defrec mkwaves 
-  (\\l (case l 
-    ([] [])
-    ([x] [])
-    ([a b | rest] (append (wave a b amplitude) (mkwaves [ b | rest ]))))))
-" in-}
   let testExp =
     case Parser.parseE testString of
       Err _ -> Debug.crash "main: bad parse"
@@ -364,14 +409,20 @@ view model =
              let (name,code) = Utils.geti (i+1) Ex.examples in
              Signal.message myMailbox.address <| UpdateModel <| \_ ->
                let exp = Utils.fromOk_ (Parser.parseE code) in
-               { name = name, code = code, exp = exp }
+               { name = name, code = code, exp = exp, textChangedAt = Nothing }
          ]
          options
     in
-    let hExp = Html.div [ Attr.id "theSourceCode" ] [ htmlOfExp testExp ] in
+    let hExp = Html.div [ Attr.id "theSourceCode" ] [ htmlOfExp model testExp ] in
+    let btn =
+      Html.button
+         [ Attr.contenteditable False
+         , Events.onClick btnMailbox.address () ]
+         [ Html.text "Reparse" ] in
     Html.node "div"
-       [ basicStyle, Attr.contenteditable True ]
-       [ dropdown, break, hExp ]
+       -- turning off contenteditable for now
+       [ basicStyle ]
+       [ dropdown, btn, break, hExp ]
   in
   body
 
@@ -383,7 +434,8 @@ port sourceCodeSignalFromJS : Signal String
 
 port sourceCodeSignalToJS : Signal ()
 port sourceCodeSignalToJS =
-  Signal.sampleOn (Time.every (1 * Time.second)) (Signal.constant ())
+  btnMailbox.signal
+  -- Signal.sampleOn (Time.every (4 * Time.second)) (Signal.constant ())
 
 main : Signal Html
 main = Signal.map view (Signal.foldp upstate initModel events)
