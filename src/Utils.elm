225c5d8a2779019a1947abe74afef5e8b8099274
Ravi Chugh <rchugh@cs.uchicago.edu>
2016-08-16 20:03:06 -0500

    moving MaybeOne to Utils before dead code is removed from Sync

--- src/Utils.elm
+++ src/Utils.elm
@@ -418,6 +418,13 @@ distance (x1,y1) (x2,y2) = sqrt <| (x2-x1)^2 + (y2-y1)^2
 distanceInt (x1,y1) (x2,y2) =
   distance (toFloat x1, toFloat y1) (toFloat x2, toFloat y2)
 
+type alias MaybeOne a = List a
+nothing               = []
+just                  = Utils.singleton
+maybeToMaybeOne mx    = case mx of
+  Nothing -> nothing
+  Just x  -> just x
+
 -- n:number -> i:[0,n) -> RGB
 numToColor n i =
   let j = round <| (i/n) * 500 in
