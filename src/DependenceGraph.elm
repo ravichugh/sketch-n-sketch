e33fba4f1c31d4ead2f80d0e72ae23176e43fdce
Ravi Chugh <rchugh@cs.uchicago.edu>
2017-01-20 16:29:12 -0600

    resolving EVars to PatternIds

--- src/DependenceGraph.elm
+++ src/DependenceGraph.elm
@@ -21,14 +21,10 @@ type alias LetOrFun = Bool -- True for ELet, False for EFun
 
 type alias PatternId = (ScopeId, List Int)
 
-type alias Nodes = Dict PatternId Ident
-
-type alias ParentTree = Dict ScopeId ScopeId
-
 type alias ScopeGraph =
-  { scopeNodes : Dict ScopeId (LetOrFun, Set PatternId)
-  , idents     : Nodes
-  , tree       : ParentTree
+  { scopeNodes   : Dict ScopeId (LetOrFun, Set PatternId)
+  , idents       : Dict PatternId Ident
+  , tree         : Dict ScopeId ScopeId
   }
 
 type BeforeAfter = Before | After
@@ -57,79 +53,131 @@ port receiveImage : (String -> msg) -> Sub msg
 strPatId (scopeId, path) =
   String.join "_" (List.map toString (scopeId :: path))
 
-updateScopeNodes patId scopeNodes =
+foldPatternsWithIds
+  : (PatternId -> Ident -> a -> a) -> ScopeId -> List Pat -> a -> a
+foldPatternsWithIds f scopeId pats init =
+  let
+    doOne f patId pat acc =
+      case pat.val of
+        PConst _ _  -> acc
+        PBase _ _   -> acc
+        PVar _ x _  -> f patId x acc
+        PAs _ x _ p -> f patId x (doOne f patId p acc)
+        PList _ ps _ Nothing _  -> doMany f patId ps acc
+        PList _ ps _ (Just p) _ -> doMany f patId (ps ++ [p]) acc
+
+    doMany f (scopeId, path) pats acc =
+      Utils.foldli (\(i, pi) -> doOne f (scopeId, path ++ [i]) pi) acc pats
+  in
+  case pats of
+    [pat] -> doOne  f (scopeId, []) pat init     -- ELet case
+    _     -> doMany f (scopeId, []) pats init    -- EFun case
+
+addVarToEnv : PatternId -> Ident -> Env -> Env
+addVarToEnv patId x env =
+  let xBindings = Maybe.withDefault [] (Dict.get x env.varBindings) in
+  { env
+     | varBindings = Dict.insert x (patId :: xBindings) env.varBindings
+     }
+
+addVarNode : PatternId -> Ident -> ScopeGraph -> ScopeGraph
+addVarNode patId x acc =
   let (scopeId, _) = patId in
-  let (letOrFun, set) = Utils.justGet_ "updateScopeNodes" scopeId scopeNodes in
-  Dict.insert scopeId (letOrFun, Set.insert patId set) scopeNodes
+  let (letOrFun, set) = Utils.justGet_ "addVarNode" scopeId acc.scopeNodes in
+  { acc
+     | scopeNodes = Dict.insert scopeId (letOrFun, Set.insert patId set) acc.scopeNodes
+     , idents = Dict.insert patId x acc.idents
+     }
+
+addScopeNode newScopeId letOrFun acc =
+  { acc
+     | scopeNodes = Dict.insert newScopeId (letOrFun, Set.empty) acc.scopeNodes
+     }
+
+addScopeEdge newScopeId currentScopeId acc =
+  { acc
+     | tree = Dict.insert newScopeId currentScopeId acc.tree
+     }
+
+resolve x env =
+  case Dict.get x env.varBindings of
+    Just (patId :: _) -> Just patId
+    _                 -> Nothing -- should only be for library funcs
 
 
 ------------------------------------------------------------------------------
 -- Computing Scope Tree and Dependency Graph
 
+type alias Env =
+  { currentScope  : ScopeId
+  , currentScopes : Set ScopeId
+  , varBindings   : Dict Ident (List PatternId)
+  }
+
+rootId : ScopeId
 rootId = 0
+
+rootPatId : PatternId
 rootPatId = (rootId, [])
 
+initEnv : Env
+initEnv =
+  { currentScope = rootId
+  , currentScopes = Set.empty
+  , varBindings = Dict.empty
+  }
+
 compute : Exp -> ScopeGraph
-compute =
-  computeScopeGraph rootId
+compute e =
+  computeScopeGraph initEnv e
     { scopeNodes = Dict.singleton rootId (True, Set.empty)
     , idents = Dict.empty
     , tree = Dict.empty
     }
 
-computeScopeGraph currentScopeId acc e =
+computeScopeGraph : Env -> Exp -> ScopeGraph -> ScopeGraph
+computeScopeGraph env exp acc =
 
-  let recurse es =
-    List.foldl (\e acc -> computeScopeGraph currentScopeId acc e) acc es in
+  let recurse es = List.foldl (computeScopeGraph env) acc es in
 
-  let newScopeId = e.val.eid in -- only if e is ELet or EFun
+  let newScopeId = exp.val.eid in -- only if e is ELet or EFun
 
-  let addScopeNode letOrFun acc =
-    { acc
-       | scopeNodes = Dict.insert newScopeId (letOrFun, Set.empty) acc.scopeNodes
-       } in
-
-  let addScopeEdge acc =
-    { acc
-       | tree = Dict.insert newScopeId currentScopeId acc.tree
-       } in
-
-  let addVar x path acc =
-    let patId = (newScopeId, path) in
-    { acc
-       | scopeNodes = updateScopeNodes patId acc.scopeNodes
-       , idents = Dict.insert patId x acc.idents
-       } in
-
-  let
-    traversePat (pat, path) acc =
-      case pat.val of
-        PConst _ _  -> acc
-        PBase _ _   -> acc
-        PVar _ x _  -> addVar x path acc
-        PAs _ x _ p -> traversePat (p, path) (addVar x path acc)
-        PList _ ps _ Nothing _  -> traversePats ps path acc
-        PList _ ps _ (Just p) _ -> traversePats (ps ++ [p]) path acc
-
-    traversePats pats path acc =
-      Utils.foldli (\(i, pi) -> traversePat (pi, path ++ [i])) acc pats
+  let updateEnv newScopeId pats env =
+    foldPatternsWithIds addVarToEnv newScopeId pats <|
+      { env | currentScope = newScopeId
+            , currentScopes = Set.insert newScopeId env.currentScopes
+            }
   in
-
-  case e.val.e__ of
+  case exp.val.e__ of
 
     ELet _ _ _ p e1 e2 _ ->
-      let acc0 = traversePat (p, []) (addScopeEdge (addScopeNode True acc)) in
-      let acc1 = computeScopeGraph currentScopeId acc0 e1 in
-      computeScopeGraph newScopeId acc1 e2
+      let acc1 =
+        acc |> addScopeNode newScopeId True
+            |> addScopeEdge newScopeId env.currentScope
+            |> foldPatternsWithIds addVarNode newScopeId [p]
+            |> computeScopeGraph env e1
+      in
+      let env1 = updateEnv newScopeId [p] env in
+      computeScopeGraph env1 e2 acc1
 
     EFun _ ps eBody _ ->
-      let acc0 = addScopeEdge (addScopeNode False acc) in
-      let acc1 = traversePats ps [] acc0 in
-      computeScopeGraph newScopeId acc1 eBody
+      let acc1 =
+        acc |> addScopeNode newScopeId False
+            |> addScopeEdge newScopeId env.currentScope
+            |> foldPatternsWithIds addVarNode newScopeId ps
+      in
+      let env1 = updateEnv newScopeId ps env in
+      computeScopeGraph env1 eBody acc1
+
+    -- NOTE: may want to put resolved PatternIds into AST (annotated EVars)
+    --
+    EVar _ x ->
+      let resolvedId = resolve x env in
+      -- let _ = Debug.log "resolve" (x, resolvedId) in
+      acc
 
     EConst _ _ _ _  -> acc
     EBase _ _       -> acc
-    EVar _ _        -> acc
 
     EApp _ e es _     -> recurse (e::es)
     EOp _ _ es _      -> recurse es
