518f63b3261a12240538cecb4908d25573c407a1
bowenwang1996 <bowenwang1996@uchicago.edu>
2017-09-28 11:29:43 -0500

    start migrating to handsontable

--- src/Native/spreadSheet.js
+++ src/Native/spreadSheet.js
@@ -1,58 +1,44 @@
-var grid = null;
+var ht = null;
     
 function render(model) {
-    var cols = model['columns'].map(JSON.parse);
-    cols.map(function(d) {d['editor'] = Slick.Editors.Text;});
-    var rows = model['rows'].map(JSON.parse);
-    var options = {
-	editable: true,
-	enableCellNavigation: true,
-	asyncEditorLoading: false,
-	autoEdit: false
-    };
+    var cols = model['columns'];
+    var rows = model['rows'];
     if (!$("#grid").length) {
-        /*var height = $(".output-panel").height();
-        var width = $(".output-panel").width();
-        var $div = $("<div>", {"id":"grid", "style": "height:" + height + "px;" + "width:" + width + "px" });
-        $(".output-panel").append($div);*/
-	$(".output-panel").attr("id", "grid");
-	grid = new Slick.Grid("#grid", rows, cols, options);
-	grid.setSelectionModel(new Slick.CellSelectionModel());
+        $("<div/>", {id : "grid"}).appendTo(".output-panel");
+        var container = document.getElementById("grid");
+	ht = new Handsontable(container, {
+            data: rows,
+            minSpareRows: 1,
+            minSpareCols: 1,
+            rowHeaders: true,
+            colHeaders: true,
+            contextMenu: true
+        });
+        ht.updateSettings({
+            afterChange: function(e) {
+                var arr = e[0];
+                var pos = [arr[0], arr[1]];
+                var cellInfo = {pos: pos, value: arr[3]};
+                console.log(cellInfo);
+                app.ports.updateCell.send(cellInfo);
+            },
+            afterOnCellMouseDown: function(e, coords) {
+                var pos = [coords.row, coords.col];
+                var cellInfo = {pos: pos, value: ""};
+                console.log(cellInfo);
+                app.ports.cellSelection.send(cellInfo);
+            }
+        });
+        ht.render();
     }
     else {
-        if (cols == grid.getColumns()) {
-            grid.setData(rows)
-            grid.render();
-        }
-        else {
-            grid.setColumns(cols);
-            grid.setData(rows);
-            grid.render();
-        }
+        data = []
+        for (var i = 0; i < rows.length; i++)
+            for (var j = 0; j < rows[i].length; j++)
+                data.push([i, j, rows[i][j]]);
+        ht.setDataAtCell(data);
+        ht.render();
     }
-    grid.onCellChange.subscribe(function(e, args) {
-        var row = Object.values(args.item);
-        var val = row[args.cell];
-        var pos = [args.row, args.cell];
-        var cellInfo = {pos: pos, value: val};
-	console.log(cellInfo);
-	app.ports.updateCell.send(cellInfo);
-    });
-    grid.onClick.subscribe(function(e, args) {
-        var row = args.row;
-        var col = args.cell;
-        var pos = [row, col];
-        var cellInfo = {pos: pos, value: ""};
-        console.log(cellInfo);
-        app.ports.cellSelection.send(cellInfo);
-    });
-}
-
-function gotoCell(cellInfo) {
-    var row = cellInfo.pos[0];
-    var col = cellInfo.pos[1];
-    grid.gotoCell(row, col, true);
 }
 
 app.ports.render.subscribe(render);
-app.ports.gotoCell.subscribe(gotoCell);
