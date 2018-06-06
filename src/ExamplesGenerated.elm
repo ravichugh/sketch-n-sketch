7cb12cc3d06f6cb270b40d4be416a09109473769
Mikaël Mayer <mikael.mayer@epfl.ch>
2018-06-06 13:52:36 -0500

    NSF proposal ready for displaying.

--- src/ExamplesGenerated.elm
+++ src/ExamplesGenerated.elm
@@ -9138,22 +9138,65 @@ slide h1, slide h2 {
 """
 
 docs =
- """-- Very useful link: http://alistapart.com/article/boom
+ """ensureNatural {minbound} n = { apply x = freeze x, update {input, outputNew} =
+  let (f, c) = (max minbound <| floor outputNew, max minbound <| ceiling outputNew) in
+  if f /= outputNew || c /= outputNew then let solutions = (if f == input then [] else [f]) ++
+  (if c == input || c == f then [] else [c]) in
+  { values = solutions } else { values = [outputNew] } }.apply n
 
-ensureInt n = { apply x = freeze x, update {input, outputNew} =
-  let f = floor outputNew in
-  let c = ceiling outputNew in
-  if f /= outputNew || c /= outputNew then
-    let solutions = (if f == input then [] else [f]) ++
-      (if c == input then [] else [c]) in
-    { values = solutions }
-  else { values = [outputNew] }
-}.apply n
+format n = { apply n = freeze (toString n |> Regex.replace \"\"\"(\\d)(?=(?:(?:\\d{3})+$))\"\"\" (\\m -> nth m.group 1 + \",\"))
+             update {outputNew} = {values=[String.toInt (Regex.replace \",\" \"\" outputNew)]}}.apply n
 
-nbpostdocs = ensureInt 5
-nbyears = ensureInt 2
-postdocsalary = 60000
+nbpostdocs     = ensureNatural {minbound=0} 2
+nbgraduates    = ensureNatural {minbound=1} 4
+nbyears        = ensureNatural {minbound=1} 2
+postdocsalary  = ensureNatural {minbound=45000} 47000
+graduatesalary = ensureNatural {minbound=25000} 28136
+salariesPerYear = postdocsalary * nbpostdocs + graduatesalary * nbgraduates
+years callback = List.range 1 nbyears |> List.map callback
+plural x = if x > 1 then \"s\" else \"\"
+pageContent = <div class=\"pagecontent\" contenteditable=\"true\">
+<h1 style=\"text-align:center\">NSF Grant proposal</h1><h2>Requested funding</h2>
+We want to sustain for @nbyears year@plural<|nbyears:
+<ul>
+  <li>@nbpostdocs postdoc@plural<|nbpostdocs paid @format<|(postdocsalary)$ per year.</li>
+  <li>@nbgraduates graduate student@plural<|nbgraduates paid @format<|(graduatesalary)$ per year.</li>
+</ul>
+Therefore, we ask for @format<|(nbyears*salariesPerYear)$.
+Detailed budget can be found in <a class=\"sectionref\" href=\"#budget\">the budget section</a>.
+<h2 id=\"budget\">Detailed budget</h2>
+  <table>
+    <tr><th>Who</th>
+    @(years <| \\i -> <th>Year #@i</th>)<th>Total</th></tr>
+    <tr><td>@nbgraduates graduates</td>
+      @(years <| \\i -> <td>@format<|(nbgraduates * graduatesalary)$</td>)
+      <td>@(years (always <| nbgraduates * graduatesalary) |> List.sum |> format)$</td></tr>
+    <tr><td>@nbpostdocs postdocs</td>
+      @(years <| \\i -> <td>@format<|(nbpostdocs * postdocsalary)$</td>)
+      <td>@(years (always <| nbpostdocs * postdocsalary) |> List.sum |> format)$</td></tr>
+    <tr><td><b>Total</b></td>
+    @(years (always <| <td>@(format salariesPerYear)$</td>))
+    <td><b>@(salariesPerYear * nbyears |> format)$</b></td>
+    </tr>
+  </table>
+  <h2>Research statement</h2>
+  We can create very nice shiny documents that can embed formulas, but we would like to research how we can<br>
+  <ul><li>Make it collaborative<br></li>
+  <li>Ensure that we can format paragraphs that contain values properly</li>
+  <li>Deal with tables nicely by back-propagating expressions</li></ul>
+</div>
 
+
+
+
+
+
+
+
+
+
+-- Very useful link: http://alistapart.com/article/boom
+-- https://code.tutsplus.com/tutorials/create-a-wysiwyg-editor-with-the-contenteditable-attribute--cms-25657
 <div id=\"app\">
 <div id=\"menu\">
 @Html.forceRefresh<|<style>
@@ -9166,8 +9209,12 @@ postdocsalary = 60000
 #menu > a:hover {
   background: #cccccc;
 }
+#menu > a ul, #menu > a ol {
+ padding-left: 0px;
+}
 #menu {
   margin-top: 5px;
+  height: 2em;
   margin-bottom-: 5px;
   border-bottom: 2px solid #cccccc;
   background: #eeeeee;
@@ -9197,32 +9244,60 @@ postdocsalary = 60000
    height: 100%;
    box-sizing: border-box;
 }
+th, td {
+    padding: 5px;
+}
+body {
+  counter-reset: h2-counter;
+}
+h2::before {
+    counter-increment: h2-counter;
+    content: counter(h2-counter) \". \";
+}
 </style>
 <b>Sketch-n-Sketch docs</b>
-<a href=\"#\" data-command='h1'>H1</a>
-<a href=\"#\" data-command='h2'>H2</a>
-<a href=\"#\" data-command='undo'>⟲ Undo</a>
-<a href=\"#\" data-command='redo'>⟳ Redo</a>
-<a href=\"#\" data-command='createlink'>Link</a>
-<a href=\"#\" data-command='justifyLeft'>Justify</a>
-<a href=\"#\" data-command='superscript'>Superscript</a></div>
+<a href=\"#\" data-command='formatBlock:h1' title=\"First-level header\">H1</a>
+<a href=\"#\" data-command='formatBlock:h2' title=\"Second-level header\">H2</a>
+<a href=\"#\" data-command='undo' style=\"display:none\">⟲ Undo</a>
+<a href=\"#\" data-command='redo' style=\"display:none\">⟳ Redo</a>
+<a href=\"#\" data-command='createlink' title=\"Create a link\">Link</a>
+<a href=\"#\" data-command='justifyLeft' title=\"Align text to the left\">Left justify</a>
+<a href=\"#\" data-command='justifyCenter' title=\"Center text\">Center</a>
+<a href=\"#\" data-command='superscript' title=\"superscript\">X<sup>b</sup></a>
+<a href=\"#\" data-command='subscript' title=\"subscript\">X<sub>b</sub></a>
+<a href=\"#\" data-command='insertUnorderedList' title=\"bullet point list\"><ul
+  style=\"font-size:0.05em;display:inline-block\"
+  ><li>---</li><li>---</li><li>---</li></ul></a>
+<a href=\"#\" data-command='insertOrderedList' title=\"numbered list\"><ol
+  style=\"font-size:0.05em;display:inline-block\"
+  ><li>---</li><li>---</li><li>---</li></ol></a>
+</div>
 @Html.forceRefresh<|<div id=\"body\">
 <div class=\"page\">
-<div class=\"pagecontent\" contenteditable=\"true\">
-NSF Grant proposal<br><br>
-We want to sustain @nbpostdocs postdocs for @nbyears years,
-each one having a salary of @(postdocsalary)$ per year.<br>
-Therefore, we ask for @(nbyears*postdocsalary*nbpostdocs)$.<br></div>
+@pageContent
 </div>
 </div>
 @Html.forceRefresh<|<script>
-var el = document.getElementById(\"body\");
+/*var el = document.getElementById(\"body\");
 var range = document.createRange();
 var sel = window.getSelection();
-range.setStart(el.childNodes[0], 5);
+range.setStart(el.childNodes[0], 0);
 range.collapse(true);
 sel.removeAllRanges();
-sel.addRange(range);
+sel.addRange(range);*/
+var as = document.querySelectorAll(\"a[data-command]\")
+for(var i = 0; i < as.length; i++) {
+  var a = as[i];
+  var c = a.dataset.command;
+  var cs = c.split(\":\")
+  var command = cs[0];
+  a.onclick = (function(command, argument) { return function() {
+    if(command == \"createlink\") {
+      argument = prompt('Enter the link here: ','http:\\/\\/');
+    }
+    document.execCommand(command, false, argument);
+  } })(cs[0], cs[1] ? cs[1] : null);
+}
 </script>
 </div>
 """
