c5ca7ee26588f1c88a1e59a973e47ff60705a549
Mikaël Mayer <mikael.mayer@epfl.ch>
2018-10-16 16:09:45 +0200

    Edit lenses updatd.

---
+++ examples/previouswork/edit-lenses-popl-2012-fig1.elm
@@ -0,0 +1,28 @@
+-- From the paper "Edit lenses" (POPL 2012)
+-- Their supported scenario is the following
+-- * Add a line Monteverdi, 1567-1643 at the end of the first list
+-- * Correct the country to "Italy" for Monteverdi on the second list
+--   Before hitting update, correct Shumann => Schumann on the second lsit
+-- * Either
+--   * Delete the Monteverdi line (first list) and reinsert it above
+--     The country will miss in the second list
+--   * (not supported in our system) reorder the elements.
+
+
+default = {name="?name?", dates="?dates?", country="?country?"}
+
+data = [
+  {name="Schubert", dates="1797-1828", country="Austria"},
+  {name="Shumann", dates="1810-1856", country="Germany"}]
+
+view1 = mapWithDefault default (
+  \{name, dates} ->   <li>@name, @dates</li>) data
+
+view2 = mapWithDefault default (
+  \{name, country} -> <li>@name, @country</li>) data
+
+Html.forceRefresh <|
+expressionFreeze <div>
+  <ul>@view1</ul>
+  <ul>@view2</ul>
+</div>
---
+++ examples/previouswork/edit-lenses-popl-2012-fig2.elm
@@ -0,0 +1,24 @@
+-- From the paper "Edit lenses" (POPL 2012), figure 2.
+-- Their edition scenario:
+-- * Insert Salinger between Kerouac and Tolstoy in the second column
+-- This inserts Inr Salinger in the original list.
+-- However, had they inserted it between Schumann and Beethoven, there would have been an ambiguity that is very well captured by our approach.
+-- Furthermore, they require to store the complement, which is built-in in our approach.
+
+tagged = [
+  Inl "Schumann",
+  Inr "Kerouac",
+  Inr "Tolstoy",
+  Inl "Beethoven"]
+
+view1 = List.filter (case of
+  Inl _ -> True; _ -> False) tagged |> List.map (\Inl x -> <li>@x</li>)
+
+view2 = List.filter (case of
+  Inr _ -> True; _ -> False) tagged |> List.map (\Inr x -> <li>@x</li>)
+  
+Html.forceRefresh <|
+expressionFreeze <div>
+  <ul style="display:inline-block">@view1</ul>
+  <ul style="display:inline-block">@view2</ul>
+</div>
