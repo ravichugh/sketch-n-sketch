16fab44bbcbee89b5ddd5372e6218aa286b42569
Justin Lubin <jlubi333@gmail.com>
2019-07-07 00:35:53 -0400

    Update listLast col2

--- src/PBESuite.elm
+++ src/PBESuite.elm
@@ -541,9 +541,9 @@ listLast xs = ??
   """specifyFunction listLast
   [ ([], None ())
   , ([1], Some 1)
-  , ([2], Some 2)
+  -- , ([2], Some 2)
   -- , ([2, 1], Some 1)
-  -- , ([1, 2], Some 2)
+  , ([1, 2], Some 2)
   , ([3, 2, 1], Some 1)
   ]""" 4
 
